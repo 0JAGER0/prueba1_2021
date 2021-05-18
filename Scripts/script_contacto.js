a6bef9dfa6aab6b6fdf46396e571824f4643089c
yurivKmirano <di.uribe@duocuc.cl>
2021-05-18 17:38:12 -0400

    nueva api agregada

--- Scripts/script_contacto.js
+++ Scripts/script_contacto.js
@@ -1,4 +1,6 @@
 
+// API SABER CLIMA 
+
 const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
 const msg = document.querySelector(".top-banner .msg");
@@ -82,3 +84,57 @@ form.addEventListener("submit", e => {
   form.reset();
   input.focus();
 });
+
+
+
+// API GEOLOCALIZACION
+
+
+let map, infoWindow;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 6,
+  });
+  infoWindow = new google.maps.InfoWindow();
+  const locationButton = document.createElement("button");
+  locationButton.textContent = "Presiona para ver tu locacion";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Locacion encontrada");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: El servicio de geolocalizacion a fallado."
+      : "Error: Tu buscador no soporta la geolocalizacion."
+  );
+  infoWindow.open(map);
+}
+
+
