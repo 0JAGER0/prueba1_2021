2649eca86c2f16b2e21a325815f821451f8a49e1
Nicolaspina337 <ni.pinan@duocuc.cl>
2021-05-20 18:42:35 -0400

    actualizacion codigo con google_map_static

--- Scripts/script_contacto.js
+++ Scripts/script_contacto.js
@@ -1,53 +1,32 @@
+//COMIENZO DE FUNCION PARA GEOLOCALIZACION (EL MAPEO_STATIC SE IMPLEMENTARA PARA PODER IMPRIMIR LA UBICACION DENTRO DE UNA IMAGEN A TRAVES DE UN BUTTON)
+function mapeo_static() {
+  var output = document.getElementById('map');
 
-// API GEOLOCALIZACION
-
-
-let map, infoWindow;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -33.4513, lng: -70.6653 },
-    zoom: 11,
-  });
+  //CONDICIONES PARA PODER SABER EL SOPORTE DE LA GEOLOCALIZACION
+  if (navigator.geolocation) {
+    output.innerHTML = "<p>Tu navegador soporta Geolocalizacion</p>";
+  } else {
+    output.innerHTML = "<p>Tu navegador no soporta Geolocalizacion</p>";
+  }
   
-  infoWindow = new google.maps.InfoWindow();
-  const locationButton = document.createElement("button");
-  locationButton.textContent = "Presiona para ver tu locacion";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  locationButton.addEventListener("click", () => {
-    
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Locacion encontrada");
-          infoWindow.open(map);
-          map.setCenter(pos);
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter());
-        }
-      );
-    } else {
-      
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
-  });
+
+  function localizacion_persona(posicion) {
+
+    var latitude = posicion.coords.latitude;
+    var longitude = posicion.coords.longitude;
+
+    //servira para poder imprimir la ubicacion dentro de un mapa con google static map
+    var imgURL = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&size=600x300&markers=color:red%7C" + latitude + "," + longitude + "&key=AIzaSyBnhlD-Nb-YLYd0NpBwOzt2aeEukHvGXxE";
+    output.innerHTML = "<img src='" + imgURL + "'>";
+
+  }
+
+  function error_ubicacion() {
+    output.innerHTML = "<p>No se pudo obtener tu ubicación</p>";
+
+  }
+
+  //servira para almacenar las variables de "localizacion_persona" y "error_ubicacion", para asi despues mostrar la ubicacion 
+  navigator.geolocation.getCurrentPosition(localizacion_persona, error_ubicacion);
+
 }
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: El servicio de geolocalizacion a fallado."
-      : "Error: Tu buscador no soporta la geolocalizacion."
-  );
-  infoWindow.open(map);
-}
-
-
